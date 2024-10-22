85c3e8c40d19a474d158f5085be7cfba88b55d4f
Sintia120804 <tia877805@gmail.com>
2024-10-22 23:22:04 +0700

    sintia

---
+++ app/src/main/java/com/sintia/project_kotlin/model/ModelBuku.kt
@@ -0,0 +1,6 @@
+package com.sintia.project_kotlin.model
+
+data class ModelBuku(
+    val judul: String,
+    val penulis: String
+)
---
+++ app/src/main/java/com/sintia/project_kotlin/model/ModelMovie.kt
@@ -0,0 +1,10 @@
+package com.sintia.project_kotlin.model
+
+data class ModelMovie(
+    var title: String,
+    var image: Int
+) {
+    class MyViewHolder {
+
+    }
+}
